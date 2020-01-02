17b95d123979097614a6244e4c94b3ec51b08d91
guangchangli <guangchangde@gmail.com>
2020-01-02 16:59:21 +0800

    hello world

---
+++ src/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Printf("hello go")
+	http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
+		s := fmt.Sprintf("你好，世界！-- Time:%s", time.Now().String())
+		fmt.Fprintf(w, "%v\n", s)
+		log.Printf("%v\n", s)
+	})
+	if err := http.ListenAndServe(":12345", nil);
+		err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
