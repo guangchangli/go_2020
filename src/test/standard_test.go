5461cd8732bbbd20663e50a8c4a86188630d111a
guangchangli <guangchangde@gmail.com>
2020-01-15 09:53:57 +0800

    add t.Helper

--- src/test/standard_test.go
+++ src/test/standard_test.go
@@ -2,14 +2,33 @@ package test
 
 import "testing"
 
-func hello() string{
+func hello() string {
 	return "hello world"
 }
 
 func Test_Standard(t *testing.T) {
-	 got:=hello()
-	 want:="hello go"
-	if got !=want {
-		t.Errorf("got '%q' want '%q'",got,want)
+	got := hello()
+	want := "hello go"
+	if got != want {
+		t.Errorf("got '%q' want '%q'", got, want)
 	}
+}
+
+const str1="hello"
+const str2="world"
+func Test_Common(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+	t.Run("compare two string", func(t *testing.T) {
+		got := "hello"
+		want := "hell"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("compare use const", func(t *testing.T) {
+		assertCorrectMessage(t,str1,str2)
+	})
 }
